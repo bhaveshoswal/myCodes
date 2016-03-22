fcff23dbcb51ac7e721d88441d1cf60801258d8e
Bhavesh Vinod Oswal <oswal.bhavesh2010@gmail.com>
2016-03-22 12:35:14 +0530

    clean
    The function deletes the 0kb files from given path if permission given

---
+++ clean.py
@@ -0,0 +1,16 @@
+import os
+
+
+def clean(path, filename=False):
+    l = os.listdir(path)
+    print("Path : ", path)
+    print("Total files are : ", len(l))
+    empty = [x for x in l if os.path.getsize(x) == 0]
+    print("Total files with 0 size : ", len(empty))
+    input = raw_input("Do you want delete empty files (y/n) : ")
+    if(input == 'y'):
+        map(os.remove, empty)
+    if(filename):
+        return empty
+    pass
+
